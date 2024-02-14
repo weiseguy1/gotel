2dca826cc78e8da49288d73d300a44ec78fb2a43
WeiseGuy <info@weiseguy.com>
2024-02-14 10:28:44 -0600

    Added boilerplate code

---
+++ cmd/web/main.go
@@ -0,0 +1,58 @@
+package main
+
+/*
+How this app compiles is it finds the import from handlers,
+then from handlers.go pulls in the dependency of render.go
+*/
+
+import (
+    "fmt"
+    "github.com/weiseguy1/gotel/pkg/config"
+    "github.com/weiseguy1/gotel/pkg/handlers"
+    "github.com/weiseguy1/gotel/pkg/render"
+    "github.com/alexedwards/scs/v2"
+    "time"
+    "log"
+    "net/http"
+)
+
+const portNumber = ":8080"
+var app config.AppConfig
+var session *scs.SessionManager
+
+func main() {
+
+    // change to true when in production
+    app.InProduction = false
+
+    session = scs.New()
+    session.Lifetime = 24 * time.Hour
+    session.Cookie.Persist = true
+    session.Cookie.SameSite = http.SameSiteLaxMode
+    session.Cookie.Secure = app.InProduction
+
+    app.Session = session
+
+    tc, err := render.CreateTemplateCache()
+    if err != nil {
+        log.Fatal("cannot create template cache")
+    }
+
+    app.TemplateCache = tc
+    app.UseCache = false
+
+    repo := handlers.NewRepo(&app)
+    handlers.NewHandlers(repo)
+    
+    render.NewTemplates(&app)
+
+    fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+
+    srv := &http.Server {
+        Addr: portNumber,
+        Handler: routes(&app),
+    }
+
+    err = srv.ListenAndServe()
+    log.Fatal(err)
+}
---
+++ pkg/handlers/handlers.go
@@ -0,0 +1,48 @@
+package handlers 
+
+import (
+    "github.com/weiseguy1/gotel/pkg/config"
+    "github.com/weiseguy1/gotel/pkg/render"
+    "github.com/weiseguy1/gotel/pkg/models"
+    "net/http"
+)
+
+
+// Repo the repository used by the handlers
+var Repo *Repository
+
+// Repository is the repository type
+type Repository struct {
+    App *config.AppConfig
+}
+
+// NewRepo creates a repository
+func NewRepo(a *config.AppConfig) *Repository {
+    return &Repository {
+        App: a,
+    }
+}
+
+// NewHandlers sets the repository for the handlers
+func NewHandlers(r *Repository) {
+    Repo = r
+}
+
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+    remoteIP := r.RemoteAddr
+    m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
+    render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
+}
+
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+    stringMap := make(map[string]string)
+    stringMap["test"] = "Hello, again."
+
+    remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+    stringMap["remote_ip"] = remoteIP
+
+    render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
+        StringMap: stringMap,
+    })
+}
